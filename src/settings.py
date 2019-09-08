39a938d11c048633964b0173e32f51e42e4b0042
Amin Abdisamad <theaminux@gmail.com>
2019-09-08 04:39:49 +0200

    Added Model class for questions and answers and also listed installed apps

--- src/settings.py
+++ src/settings.py
@@ -30,6 +30,7 @@ sentry_sdk.init(
 # Application definition
 
 INSTALLED_APPS = [
+    'vote.app.VoteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- vote/models.py
+++ vote/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Questions(models.Model):
+    question_text = models.CharField(max_legth=200)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models(models.ForeignKey(
+        "question_text",  on_delete=models.CASCADE))
+    choice_text = models.models.CharField(max_length=100)
+    vote = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
