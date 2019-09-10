879d7fddf2b90fd8bc83dd49cfc57d039d1453c0
Amin Abdisamad <theaminux@gmail.com>
2019-09-10 13:06:46 +0200

    changed header and added model admin

--- vote/admin.py
+++ vote/admin.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 from .models import Questions, Choice
 # Register your models here.
 
-admin.site.register(Questions)
-admin.site.register(Choice)
+# change site header
+admin.site.site_header = "Voting System"
+
+admin.site.site_title = "Voting System Area"
+admin.site.index_title = "Voting Admin Area"
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}),
+                 ('Date Information', {'fields': [
+                  'pub_date'], 'classes':['collapse']}),
+                 ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Questions, QuestionAdmin)
+# admin.site.register(Questions)
