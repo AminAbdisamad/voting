b9bbc33cbfe572f147d71914959f2d6b7c5385c3
Amin Abdisamad <theaminux@gmail.com>
2019-09-11 13:22:19 +0200

    added detail view and the root for it

--- vote/urls.py
+++ vote/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'vote'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:question_id>', views.index, name='detail')
 ]
--- vote/views.py
+++ vote/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import Http404
 from .models import Questions, Choice
 
 
@@ -7,3 +7,11 @@ def index(request):
     latestQuestionList = Questions.objects.order_by('-pub_date')[:5]
     context = {"latestQuestionList": latestQuestionList}
     return render(request, 'vote/index.html', context)
+
+
+def detail(request, id):
+    try:
+        questionId = Questions.objects.get(pk=id)
+    except Questions.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "vote/detail.html", {"questionId": questionId})
